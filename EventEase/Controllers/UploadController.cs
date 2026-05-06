164c264a2e98d4818f50df687dd17565e1db9488
Wenzile Nosipho Mbele <ST10104037@rcconnect.edu.za>
2026-05-06 20:47:08 +0200

    blob storage

---
+++ EventEase/Controllers/UploadController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EventEase.Services;
+
+namespace EventEase.Controllers
+{
+    public class UploadController : Controller
+    {
+        private readonly BlobStorageService _blobService;
+
+        public UploadController()
+        {
+            _blobService = new BlobStorageService();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadImage(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("No file selected");
+
+            var imageUrl = await _blobService.UploadAsync(file);
+
+            return Ok(new { url = imageUrl });
+        }
+    }
+}
---
+++ EventEase/Services/BlobStorageService.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Storage.Blobs;
+
+namespace EventEase.Services
+{
+    public class BlobStorageService
+    {
+        private readonly string connectionString = "Server=tcp:eventease1.database.windows.net,1433;Initial Catalog=EventEaseDB;Persist Security Info=False;User ID=ST10104037;Password=EventEase123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;";
+        private readonly string containerName = "images";
+
+        public async Task<string> UploadAsync(IFormFile file)
+        {
+            var blobServiceClient = new BlobServiceClient(connectionString);
+            var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            var blobClient = containerClient.GetBlobClient(file.FileName);
+
+            using (var stream = file.OpenReadStream())
+            {
+                await blobClient.UploadAsync(stream, true);
+            }
+
+            return blobClient.Uri.ToString();
+        }
+    }
+}
